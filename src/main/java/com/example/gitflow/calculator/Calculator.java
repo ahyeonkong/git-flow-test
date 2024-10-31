d469364fc58c9d3aa3909d0caaa8ec8ee21397c9
ahyeonkong <kimah0403@gmail.com>
2024-10-31 16:09:09 +0900

    덧셈 기능 추가

--- src/main/java/com/example/gitflow/calculator/Calculator.java
+++ src/main/java/com/example/gitflow/calculator/Calculator.java
@@ -4,4 +4,8 @@ public class Calculator {
     public static void main(String[] args) {
         System.out.println("계산기 애플리케이션 v3.0");
     }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
 }
